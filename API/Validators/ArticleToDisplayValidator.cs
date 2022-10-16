9941a1497e748b757ec57b066a0e431761a8425e
Dana <Danajayousi744@gmail.com>
2022-10-16 22:16:51 +0300

    add validator

---
+++ API/Validators/ArticleToDisplayValidator.cs
@@ -0,0 +1,17 @@
+﻿using API.Models;
+using FluentValidation;
+
+namespace API.Validators;
+
+public class ArticleToDisplayValidator : AbstractValidator<ArticleToUpsertDto>
+{
+    public ArticleToDisplayValidator()
+    {
+        RuleFor(article => article.Title)
+            .NotEmpty()
+            .WithMessage("Title cannot be empty");
+        RuleFor(article => article.Content)
+            .NotEmpty()
+            .WithMessage("Content cannot be empty");
+    }
+}
