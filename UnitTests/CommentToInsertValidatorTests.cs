e42c7ebc2dbb898d08f4e91a7d01ac75c9dd3183
Dana <Danajayousi744@gmail.com>
2022-10-18 03:51:17 +0300

    add validator test

---
+++ UnitTests/CommentToInsertValidatorTests.cs
@@ -0,0 +1,30 @@
+﻿using API.Models;
+using API.Validators;
+using FluentValidation.TestHelper;
+
+namespace UnitTests;
+
+public class CommentToInsertValidatorTests
+{
+    private readonly CommentToInsertValidator _validator;
+
+    public CommentToInsertValidatorTests()
+    {
+        _validator = new CommentToInsertValidator();
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    public void RuleForContent_WhenContentIsNullOrEmpty(string Content)
+    {
+        var commentToInsertDto = new CommentToInsertDto
+        {
+            content = Content
+        };
+        var result = _validator.TestValidate(commentToInsertDto);
+        result
+            .ShouldHaveValidationErrorFor(dto => dto.content)
+            .WithErrorMessage("Comment cannot be empty");
+    }
+}
